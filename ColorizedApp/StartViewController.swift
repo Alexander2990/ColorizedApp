edb744a9b4c7e6984abc08c5a9e799e02d1d0003
Alexander <Alexander29_90@icloud.com>
2024-08-28 18:13:41 +0500

    Rename ViewController. Added StartViewController

---
+++ ColorizedApp/StartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StartViewController.swift
+//  ColorizedApp
+//
+//  Created by Александр on 28.08.2024.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
